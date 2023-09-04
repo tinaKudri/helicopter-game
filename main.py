30a2d9906b7b8e50e7a485cb72d1011d1c28f7fe
tinaKudri <lena.mat24@mail.ru>
2023-09-04 13:28:46 +0300

    deploy game

---
+++ main.py
@@ -0,0 +1,48 @@
+# 🌲 🌊 🚁 🟩 🔥 🏥 💛 💎 🪣 🏦 🌥️ ⚡ 🏆 ⬛
+
+from pynput import keyboard
+from map import Map
+import time
+import os
+from helicopter import Helicopter as Helico
+
+TICK_SLEEP = 0.05
+TREE_UPDATE = 50
+FIRE_UPDATE = 100
+MAP_W, MAP_H = 20, 10
+
+field = Map(MAP_W, MAP_H)
+
+helico = Helico(MAP_W, MAP_H)
+
+
+MOVES = {'w': (-1, 0), 'd': (0, 1), 's': (1, 0), 'a': (0, -1)}
+def process_key(key):
+    global helico
+    c = key.char.lower()
+    if c in MOVES.keys():
+        dx, dy = MOVES[c][0], MOVES[c][1]
+        helico.move(dx, dy)
+listener = keyboard.Listener(
+    on_press=None,
+    on_release=process_key,)
+listener.start()
+
+
+
+
+
+tick = 1
+
+while True:
+    os.system("clear") 
+    print("TICK", tick)
+    field.process_helicopter(helico)
+    helico.print_stats()
+    field.print_map(helico)
+    tick += 1
+    time.sleep(TICK_SLEEP)
+    if (tick % TREE_UPDATE == 0):
+        field.generate_tree()
+    if (tick % FIRE_UPDATE == 0):
+        field.update_fires()
---
+++ map.py
@@ -0,0 +1,102 @@
+
+from utils import randbool
+from utils import randcell
+from utils import randcell2
+
+# 0 - поле
+# 1 - дерево
+# 2 - река
+# 3 - госпиталь
+# 4 - апгрейд-шоп
+# 5 - огонь
+
+CELL_TYPES = "🟩🌲🌊🏥🏦🔥"
+THREE_BONUS = 100
+#TODO change 5000
+UPGRADE_COST = 500
+
+
+class Map:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.cells =[[0 for i in range(w)] for j in range(h)]
+        self.generate_forest(3, 10)
+        self.generate_river(10)
+        self.generate_river(10)
+        self.generate_upgrade_shop()
+
+    def chek_bounds(self, x, y):
+        if (x < 0 or y < 0 or x >= self.h or y >= self.w):
+            return False
+        return True  
+
+    def print_map(self, helico):
+        print("⬛" * (self.w + 2))
+        for ri in range(self.h):
+            print("⬛", end="")
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if (helico.x == ri and helico.y == ci):
+                    print("🚁", end="")
+                elif (cell >= 0 and cell < len(CELL_TYPES)):
+                    print(CELL_TYPES[cell], end="")
+            print("⬛")
+        print("⬛" * (self.w + 2))
+
+    def generate_river(self, l):
+        rc = randcell(self.w, self.h)
+        rx, ry = rc[0], rc[1]
+        self.cells[rx][ry] = 2
+        while l > 0:
+            rc2 = randcell2(rx, ry)
+            rx2, ry2 = rc2[0], rc2[1]
+            if (self.chek_bounds(rx2,ry2)):
+                self.cells[rx2][ry2] = 2
+                rx, ry = rx2, ry2
+                l -= 1
+    def generate_forest(self, r, mxr):
+        for ri in range (self.h):
+            for ci in range(self.w):
+                if randbool(r, mxr):
+                    self.cells[ri][ci] = 1
+    def generate_tree(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        if (self.cells[cx][cy] == 0):
+            self.cells[cx][cy] = 1
+    def generate_upgrade_shop(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        self.cells[cx][cy] = 4
+
+    
+    def add_fire(self):
+            c = randcell(self.w, self.h)
+            cx, cy = c[0], c[1]
+            if self.cells[cx][cy] == 1:
+                self.cells[cx][cy] == 5    
+    def update_fires(self):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if cell == 5:
+                    self.cells[ri][ci] = 0
+        for i in range(10):
+            self.add_fire()
+
+
+    def process_helicopter(self, helico):
+        c = self.cells[helico.x][helico.y]
+        if (c == 2):
+            helico.tank = helico.mxtank
+        elif (c == 5 and helico.tank > 0):
+           helico.tank -= 1
+           helico.score += THREE_BONUS
+           self.cells[helico.x][helico.y] = 1
+        if (c == 4 and helico.score >= UPGRADE_COST):
+            helico.mxtank += 1
+            helico.score -= UPGRADE_COST
+
+
+    
